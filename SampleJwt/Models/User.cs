4404d770a5241bb75bed8d556008338790fb9130
Gamal Mohamed <GamalMohamed@gmail.com>
2023-02-01 19:44:56 +0200

    Add project files.

---
+++ SampleJwt/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleJwt.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public User(string username, string password)
+        {
+            UserName = username;
+            Password = password;
+        }
+    }
+}
